8d1af1194664078bfe36a17275c4cc8768430861
GUILHERME ALBINO <guilhermealbino33@gmail.com>
2026-02-07 08:10:02 -0300

    chore: add eslint and prettier

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['../.eslintrc.json'],
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    // Add React specific settings when React is installed
+    // extends: ['../.eslintrc.json', 'plugin:react/recommended'],
+};
