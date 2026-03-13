de9573b3d919c115fc271858acea1e7b1bdbfbff
GUILHERME ALBINO <guilhermealbino33@gmail.com>
2026-03-12 22:32:29 -0300

    chore: add todo comments

--- backend/src/crosscutting/database/migrations/1772060000000-AddLidIdentifierToChatbotUser.ts
+++ backend/src/crosscutting/database/migrations/1772060000000-AddLidIdentifierToChatbotUser.ts
@@ -3,6 +3,11 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddLidIdentifierToChatbotUser1772060000000 implements MigrationInterface {
   name = 'AddLidIdentifierToChatbotUser1772060000000';
 
+  /**
+   *
+   * @todo: analisar se esta migration é necessária
+   */
+
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "chatbot_users" ADD "lid_identifier" character varying`);
   }
